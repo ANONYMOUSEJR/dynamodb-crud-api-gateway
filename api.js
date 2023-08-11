0cc7a2d15ddee49abf58f38b6d36e43d6b535dc8
ANONYMOUSEJR <64444223+ANONYMOUSEJR@users.noreply.github.com>
2023-08-11 07:12:12 +0400

    Made the db work with products.

--- api.js
+++ api.js
@@ -1,70 +1,114 @@
+/* 
+Marshall: turns functions from native JS to the AWS thingy.
+UnMarshall: opposite of marshall, lol.
+*/
+
 const db = require("./db");
 const {
+    /*
     GetItemCommand,
     PutItemCommand,
     DeleteItemCommand,
     ScanCommand,
     UpdateItemCommand,
+    */
+    getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    getAllProducts,
+    GetItemCommand,
+    DeleteItemCommand,
+    ScanCommand,
 } = require( "@aws-sdk/client-dynamodb" );
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 
-const getPost = async (event) => {
-    const response = { statusCode: 200 };
+const getProduct = async (event) => {
+    const response = { statusCode: 200 }; // OK.
 
     try {
         const params = {
             TableName: process.env.DYNAMO_TABLE_NAME,
-            Key: marshall({ productId: event.pathParameters.productId }),
+            kay: marshall({ ID: event.pathParameters.ID }),
         };
         const { Item } = await db.send( new GetItemCommand(params))
 
         console.log({ Item });
         response.body = JSON.stringify({
-            message: "Successfully retrieved post!",
+            message: "Successfully retrieved product!",
             data: (Item) ? unmarshall(Item) : {},
-            rawData: Item,
+            rawData:Item,
         })
     } catch (e) {
         console.error(e);
         response.statusCode = 500;
         response.body = JSON.stringify({
-            message: "Failed to get post!",
+            message: "Failed to get product!",
             errorMsg: e.message,
             errorStack: e.Stack,
-        });
+        })
     }
 
     return response;
 };
 
-const createPost = async (event) => {
-    const response = { statusCode: 200 };
+const createProduct = async (event) => {
+    const response = { statusCode: 200 }; // OK.
 
     try {
         const body = JSON.parse(event.body);
         const params = {
             TableName: process.env.DYNAMO_TABLE_NAME,
-            Item: marshall(body || {}),
+            Item: marshall( body || {} ),
         };
-        const createResult = await db.send( new PutItemCommand(params));
-
+        const createItem = await db.send( new PutItemCommand(params) );
+        
         response.body = JSON.stringify({
-            message: "Successfully created post!",
-            createResult,
+            message: "Successfully created product!",
+            createItem,
         })
     } catch (e) {
         console.error(e);
         response.statusCode = 500;
         response.body = JSON.stringify({
-            message: "Failed to create post!",
+            message: "Failed to create product!",
             errorMsg: e.message,
             errorStack: e.Stack,
-        });
+        })
     }
 
     return response;
 };
 
+const updateProduct = async (event) => {
+    const response = { statusCode: 200 }; // OK.
+
+    try {
+        const body = JSON.parse(event.body);
+        const params = {
+            TableName: process.env.DYNAMO_TABLE_NAME,
+            Item: marshall( body || {} ),
+        };
+        const createItem = await db.send( new PutItemCommand(params) );
+        
+        response.body = JSON.stringify({
+            message: "Successfully created product!",
+            createItem,
+        })
+    } catch (e) {
+        console.error(e);
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: "Failed to create product!",
+            errorMsg: e.message,
+            errorStack: e.Stack,
+        })
+    }
+
+    return response;
+};
+
+/*
 const updatePost = async (event) => {
     const response = { statusCode: 200 };
 
@@ -102,42 +146,45 @@ const updatePost = async (event) => {
 
     return response;
 };
+*/
 
-const deletePost = async (event) => {
-    const response = { statusCode: 200 };
+
+
+const deleteProduct = async (event) => {
+    const response = { statusCode: 200 }; // OK.
 
     try {
         const params = {
             TableName: process.env.DYNAMO_TABLE_NAME,
-            Key: marshall({ productId: event.pathParameters.productId }),
+            Key: marshall({ ID: event.pathParameters.ID }),
         };
-        const updateResult = await db.send( new DeleteItemCommand(params));
-
+        const updateResult = await db.send( new DeleteItemCommand(params) );
+        
         response.body = JSON.stringify({
-            message: "Successfully deleted post!",
+            message: "Successfully deleted product!",
             createResult,
         })
     } catch (e) {
         console.error(e);
         response.statusCode = 500;
         response.body = JSON.stringify({
-            message: "Failed to delete post!",
+            message: "Failed to delete product!",
             errorMsg: e.message,
             errorStack: e.Stack,
-        });
+        })
     }
 
     return response;
 };
 
-const getAllPost = async (event) => {
-    const response = { statusCode: 200 };
+const getAllProducts = async (event) => {
+    const response = { statusCode: 200 }; // OK.
 
     try {
         const { Items } = await db.send( new ScanCommand({ TableName: process.env.DYNAMO_TABLE_NAME }));
-
+        
         response.body = JSON.stringify({
-            message: "Successfully retrieved all post!",
+            message: "Successfully retrieved all products!",
             data: Items.map((item) => unmarshall(item)),
             Items,
         })
@@ -145,19 +192,19 @@ const getAllPost = async (event) => {
         console.error(e);
         response.statusCode = 500;
         response.body = JSON.stringify({
-            message: "Failed to retriev all post!",
+            message: "Failed to retrieve all products!",
             errorMsg: e.message,
             errorStack: e.Stack,
-        });
+        })
     }
 
     return response;
 };
 
 module.exports = {
-    getPost,
-    createPost,
-    updatePost,
-    deletePost,
-    getAllPost,
+    getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    getAllProducts,
 }
